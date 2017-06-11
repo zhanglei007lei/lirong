018959cb8348707d94b78a2fae7d6752dc60736b
zhanglei007lei <1250253900@qq.com>
2017-06-11 16:07:08 +0800

    commit this project to github

---
+++ jihe/src/com/jihe/TestArraylist.java
@@ -0,0 +1,10 @@
+package com.jihe;
+
+import java.util.ArrayList;
+
+public class TestArraylist {
+	public static void main(String[] args) {
+		ArrayList at = new ArrayList();
+		System.out.println(at.size());
+	}
+}
